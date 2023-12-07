cb1d3a198455f94504a1b67db981fd014600add5
Eiji Toriki <ageForEngi@outlook.jp>
2023-12-07 22:07:09 +0900

    JUnit 第一章演習

--- JUnitPracticeStarter/src/test/java/org/example/BusinessDayServiceTest.java
+++ JUnitPracticeStarter/src/test/java/org/example/BusinessDayServiceTest.java
@@ -1,18 +1,75 @@
 package org.example;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
 class BusinessDayServiceTest {
     private final BusinessDayService service = new BusinessDayService();
 
-    // TODO Hands-On 1-1
+    @Test
+    public void testIsBusinessDay() {
+    	boolean actual = service.isBusinessDay(LocalDate.of(2022, 10, 20));
+		assertTrue(actual);
+	}
 
-    // TODO Hands-On 1-2
+    @Test
+    public void testHolidayIsNotBusinessDay() {
+    	boolean actual = service.isBusinessDay(LocalDate.of(2022, 11, 3));
+		assertFalse(actual);
+	}
+    
+    @Test
+    public void testSaturdayIsNotBusinessDay() {
+    	boolean actual = service.isBusinessDay(LocalDate.of(2022, 12, 17));
+		assertFalse(actual);
+	}
+    
+    @Test
+    public void testSundayIsNotBusinessDay() {
+    	boolean actual = service.isBusinessDay(LocalDate.of(2022, 12, 25));
+		assertFalse(actual);
+	}
+    
+    @Test
+    public void testGetNextBusinessDayIsNull() {
+    	assertNull(service.getNextBusinessDay(LocalDate.of(2022, 8, 11), 5));
+    }
+    
+    @Test
+    public void testGetNextBusinessDay() {
+    	assertNotNull(service.getNextBusinessDay(LocalDate.of(2022, 9, 23), 5));
+    	
+    	LocalDate expecteDate = LocalDate.of(2022, 9, 26);
+    	LocalDate actualDate = service.getNextBusinessDay(LocalDate.of(2022, 9, 23), 5);
+    	assertEquals(expecteDate, actualDate);
+    }
+    
+    @Test
+    public void testGetBusinessDayArray() {
+    	
+    	LocalDate[] expecteDates = new LocalDate[] {
+    		LocalDate.of(2022, 10, 7),
+    		LocalDate.of(2022, 10, 11),
+    		LocalDate.of(2022, 10, 12)
+    	};
+    	LocalDate[] actualDates = service.getBusinessDayArray(LocalDate.of(2022, 10, 7), 5);
+    	assertArrayEquals(expecteDates, actualDates);
+    }
 
-    // TODO Hands-On 2-2
-
-    // TODO Hands-On 1-2
-
-    // TODO Hands-On 3-1
-
-    // TODO Hands-On 3-2
-
+    @Test
+    public void testGetBusinessDayList() {
+    	List<LocalDate> expecteDates = Arrays.asList(
+    		LocalDate.of(2022, 10, 7),
+    		LocalDate.of(2022, 10, 11),
+    		LocalDate.of(2022, 10, 12)
+    	);
+    	List<LocalDate> actualDates = service.getBusinessDayList(LocalDate.of(2022, 10, 7), 5);
+    	assertIterableEquals(expecteDates, actualDates);
+    }
+    
 }
--- JUnitPracticeStarter/src/test/java/org/example/CalculatorTest.java
+++ JUnitPracticeStarter/src/test/java/org/example/CalculatorTest.java
@@ -1,6 +1,28 @@
 package org.example;
 
-public class CalculatorTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+	@Test
+	public void testTaxPrice() {
+		// オブジェクトの生成
+		Calculator calculator = new Calculator();
+		// 期待値
+		int expected = 110;
+		// 実測値
+		int actual = calculator.taxPrice(100);
+		// 期待値と実測値の比較
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testTaxPriceException() {
+		// オブジェクトの生成
+		Calculator calculator = new Calculator();
+		// 例外テスト
+		assertThrows(IllegalArgumentException.class, () -> calculator.taxPrice(-110));
+	}
 
 }
